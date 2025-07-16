671ff1a968b12c0e581418624c92246dac2b0e82
kotonohafarm <kotonohafarm@gmail.com>
2025-07-16 19:42:03 +0900

    Allow Sanity image domain in Next.js config

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import { createClient } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '8nksb1fd';
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'prodaction';
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03';
 
 export const client = createClient({
